6c17d65e80a149c225986bc54026e2c2b7da4484
munmo <sunahjung7@gmail.com>
2023-09-14 13:38:39 +1000

    question 1 completed

--- climate_q1.py
+++ climate_q1.py
@@ -1,8 +1,21 @@
 import matplotlib.pyplot as plt
-        
+import sqlite3
+
 years = []
 co2 = []
 temp = []
+
+connection = sqlite3.connect("climate.db")
+cursor = connection.cursor()
+
+for row in cursor.fetchall():
+    years.append(row[0])
+    temp.append(row[1])
+    co2.append(row[2])
+
+connection.commit()
+connection.close()
+
 
 plt.subplot(2, 1, 1)
 plt.plot(years, co2, 'b--') 
